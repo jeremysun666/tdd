8c9da3370626ff10647be5901802277de63f628b
Jeremy Sun <yunxuan_sun@apple.com>
2020-07-20 15:29:36 -0700

    plus service

---
+++ src/main/java/com/demo/tdd/services/PlusService.java
@@ -0,0 +1,5 @@
+package com.demo.tdd.services;
+
+public interface PlusService {
+	int plus(int a, int b);
+}
---
+++ src/main/java/com/demo/tdd/services/PlusServiceImpl.java
@@ -0,0 +1,8 @@
+package com.demo.tdd.services;
+
+public class PlusServiceImpl implements PlusService {
+	@Override
+	public int plus(int a, int b) {
+		return a + b;
+	}
+}
