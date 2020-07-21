971f1f945e4d13b015b6f660b1b0fa0b47cc30a0
Jeremy Sun <yunxuan_sun@apple.com>
2020-07-20 17:21:39 -0700

    Multiply service

---
+++ src/main/java/com/demo/tdd/services/MultiplyService.java
@@ -0,0 +1,5 @@
+package com.demo.tdd.services;
+
+public interface MultiplyService {
+	int multiply(int a, int b);
+}
---
+++ src/main/java/com/demo/tdd/services/MultiplyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.demo.tdd.services;
+
+public class MultiplyServiceImpl implements MultiplyService {
+	@Override
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+}
